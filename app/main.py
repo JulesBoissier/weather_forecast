aa7b9acc1b3224a9a4483f3edbbb1a2e9134dcf0
Jules Boissier <boissier.jules@gmail.com>
2024-07-02 15:32:37 +0100

    Adapted get_city_data logic to refacored OpenWeatherClient

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 import os
 
+from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 import uvicorn
 
-from app.clients.open_weather_client import get_weather
+from app.clients.open_weather_client import OpenWeatherClient
 
+load_dotenv(find_dotenv())
+
+API_KEY = os.getenv("OPEN_WEATHER_KEY")
 
 app = FastAPI()
 
 
 @app.get("/city_weather")
 def get_city_data(city_name: str, date: str):
-    data = get_weather(date=date, city_name=city_name)
+    client = OpenWeatherClient(api_key=API_KEY)
+    data = client.get_weather(date = date, city_name=city_name)
     return data
 
 
